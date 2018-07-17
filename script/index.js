a32eb038484c87b9e6bbbdc5f32930e2493c7c3f
ASER <aser@ASER-iMac.local>
2018-07-17 22:46:18 +0800

    ii

---
+++ script/index.js
@@ -0,0 +1,19 @@
+var app =new Vue({
+    el:"#_page",
+    props:{
+
+    },
+
+    data:{
+        title:"First Vue.js"
+    },
+    methods:{
+
+    },
+    watch:{
+
+    },
+    computed:{
+
+    }
+})
